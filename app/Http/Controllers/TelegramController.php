c577f80f200e98ef6ac82941d6f908ae4e4853e2
Adukova Margarita <adukova_m@100points.ru>
2025-10-26 01:04:07 +0700

    fix config

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,7 +9,7 @@ class TelegramController extends Controller
 {
     public function handle(Request $request)
     {
-        $telegram = new Api(config('telegram.bots.mybot.token'));
+        $telegram = new Api(config('telegram.bots.main.token'));
         $update = $telegram->getWebhookUpdate();
 
         $chatId = $update->getMessage()->getChat()->getId();
--- config/telegram.php
+++ config/telegram.php
@@ -31,22 +31,16 @@ return [
     |             ]
     */
     'bots' => [
-        'mybot' => [
+        'main' => [
             'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
-            /*
-             * @see https://core.telegram.org/bots/api#update
-             */
+            'webhook_url' => env('APP_URL', 'http://localhost').'/webhook/telegram',
+
             'allowed_updates' => null,
             'commands' => [
                 // Acme\Project\Commands\MyTelegramBot\BotCommand::class
             ],
         ],
-
-        //        'mySecondBot' => [
-        //            'token' => '123456:abc',
-        //        ],
     ],
 
     /*
