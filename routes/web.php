d6a0ec42e3b5f666dddacea0036bc01f9d816196
Adukova Margarita <adukova_m@100points.ru>
2025-10-26 01:52:04 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\TelegramController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    \Illuminate\Support\Facades\Log::debug('meow meow');
     return 'TFF_bot';
 });
 
-
-Route::post('/webhook/telegram', [TelegramController::class, 'handle']);
