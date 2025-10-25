34adaf49a96e2d2b42bc2224bdf493a0ecb491ea
Adukova Margarita <adukova_m@100points.ru>
2025-10-26 01:22:15 +0700

    add dubug log

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Api;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
     public function handle(Request $request)
     {
-        $telegram = new Api(config('telegram.bots.mybot.token'));
-        $update = $telegram->getWebhookUpdate();
-
-        $chatId = $update->getMessage()->getChat()->getId();
-        $text = $update->getMessage()->getText();
-
-        $telegram->sendMessage([
-            'chat_id' => $chatId,
-            'text' => $text,
-        ]);
+        $updates = Telegram::getWebhookUpdate();
+        Log::debug('debug webhook', [$updates]);
     }
 }
