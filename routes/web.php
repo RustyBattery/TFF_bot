7229108e3c8f353664cb19d37b512487e9ccf96d
Adukova Margarita <adukova_m@100points.ru>
2025-10-26 02:06:01 +0700

    fix routes && add without middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TelegramController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,3 +9,4 @@ Route::get('/', function () {
     return 'TFF_bot';
 });
 
+Route::post('/webhook/telegram', [TelegramController::class, 'handle'])->withoutMiddleware(VerifyCsrfToken::class);
