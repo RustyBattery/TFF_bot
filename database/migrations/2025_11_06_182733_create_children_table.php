65f211879d14f235f25388f6ca195456faf32e39
Adukova Margarita <adukova_m@100points.ru>
2025-11-07 23:27:51 +0700

    fix migration

--- database/migrations/2025_11_06_182733_create_children_table.php
+++ database/migrations/2025_11_06_182733_create_children_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('children', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->date('birthdate')->nullable();
             $table->unsignedBigInteger('area_id')->nullable();
             $table->foreign('area_id')->references('id')->on('areas');
