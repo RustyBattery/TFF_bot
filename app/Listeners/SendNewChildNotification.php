09a45b34a9c99263554f4502eb920ad1d220cf80
Adukova Margarita <adukova_m@100points.ru>
2025-11-08 23:55:11 +0700

    fix SendNewChildNotification

--- app/Listeners/SendNewChildNotification.php
+++ app/Listeners/SendNewChildNotification.php
@@ -3,26 +3,36 @@
 namespace App\Listeners;
 
 use App\Events\ChildRegistered;
+use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Telegram\Bot\Api;
 
 class SendNewChildNotification
 {
-    /**
-     * Create the event listener.
-     */
-    public function __construct()
+    protected Api $telegram;
+
+    public function __construct(Api $telegram)
     {
-        //
+        $this->telegram = $telegram;
     }
 
-    /**
-     * Handle the event.
-     */
     public function handle(ChildRegistered $event): void
     {
         $child = $event->child;
         $user = $child->users()->first();
 
+        $text = "<b>Зарегестрирован ребенок</b>\n\n";
+        $text .= "ФИО: " . $child->name . "\n";
+        $age = Carbon::parse($child->birthdate)->age;
+        $text .= "Возраст: " . $age . "\n";
+        $text .= "Район: " . $child->area->name . "\n";
+        $text .= "Контакт родителя: @" . $user->username . "\n";
+
+        $this->telegram->sendMessage([
+            'chat_id' => env('TELEGRAM_CHANEL_ID', ''),
+            'text' => $text,
+            'parse_mode' => 'HTML',
+        ]);
     }
 }
