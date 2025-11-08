1b249ed1e0efe2391314ce7d2ff5c018e70f06a9
Adukova Margarita <adukova_m@100points.ru>
2025-11-08 20:47:27 +0700

    fix schedule callback

--- app/Telegram/Callbacks/Schedule/ScheduleCallback.php
+++ app/Telegram/Callbacks/Schedule/ScheduleCallback.php
@@ -2,6 +2,8 @@
 
 namespace App\Telegram\Callbacks\Schedule;
 
+use App\Models\Area;
+use App\Models\Lesson;
 use App\Models\Telegram\UserState;
 use App\Telegram\Callbacks\Callback;
 
@@ -14,8 +16,28 @@ class ScheduleCallback extends Callback
         $user = $this->userService->findUserByChatId($this->chatId);
         $this->userService->resetState($user);
 
+        $areas = Area::all();
+
+        $text = '';
+        foreach ($areas as $area) {
+            $text .= "<b>" . $area->name . "</b>\n<i>" . $area->address . "</i>\n";
+            foreach (Lesson::DAY_ORDER as $day) {
+                $lessons = $area->lessons()
+                    ->where('day', $day)
+                    ->orderBy('start_time')->get();
+                if (count($lessons) > 0) {
+                    $text .= "<b>". Lesson::getDayName($day) . "</b>\n";
+                }
+                foreach ($lessons as $lesson) {
+                    $text .= $lesson->getTimeRangeAttribute() . "\n";
+                }
+            }
+            $text .= "\n\n";
+        }
+
         $this->replyWithMessage([
-            'text' => 'Позже здесь будет доступен просмотр расписания'
+            'text' => $text,
+            'parse_mode' => 'HTML'
         ]);
     }
 }
